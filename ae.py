31017c4efe152d63fd07600ef5ae821f259e0b4e
vector-1127 <2424833259@qq.com>
2017-07-26 19:13:18 +0800

    Update ae.py

--- ae.py
+++ ae.py
@@ -8,6 +8,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+import numpy as np
 
 X = np.random.rand(1000,784) # 1000个784维的数据
 
